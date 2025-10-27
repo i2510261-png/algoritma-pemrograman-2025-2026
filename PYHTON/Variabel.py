0730e4db9f5616958dc9cc8434f7e30e312e2ede
intan khalimatu <i.2510261@unida.ac.id>
2025-10-27 08:19:54 +0700

    latihan2

---
+++ PYHTON/Variabel.py
@@ -0,0 +1,11 @@
+print('---Belajar Variabel---')
+char = 'A'
+string = "intan khalimatu"
+a = 18
+penghasilan1 = 1000000
+nilai_uts = 82.90
+print(char)
+print(string)
+print(a)
+print(penghasilan1)
+print(nilai_uts)
